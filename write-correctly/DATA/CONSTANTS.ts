86ac85f2f43d4ba06654b2d30554bc634038d152
AleksBartov <37273772+AleksBartov@users.noreply.github.com>
2024-12-20 01:30:45 +0300

    great work has done

---
+++ write-correctly/DATA/CONSTANTS.ts
@@ -0,0 +1,26 @@
+export const WORDS = [
+  {
+    WORD_TO_TEST: "МОЛОКО",
+    TEST_POSITION: 3,
+    ANSWER_RIGTH: "О",
+    ANSWER_WRONG: "А",
+  },
+  {
+    WORD_TO_TEST: "ХОЛОД",
+    TEST_POSITION: 3,
+    ANSWER_RIGTH: "О",
+    ANSWER_WRONG: "А",
+  },
+  {
+    WORD_TO_TEST: "ГРИБ",
+    TEST_POSITION: 3,
+    ANSWER_RIGTH: "Б",
+    ANSWER_WRONG: "П",
+  },
+  {
+    WORD_TO_TEST: "ПРИЧАЛ",
+    TEST_POSITION: 2,
+    ANSWER_RIGTH: "И",
+    ANSWER_WRONG: "Е",
+  },
+];
--- write-correctly/app/index.tsx
+++ write-correctly/app/index.tsx
@@ -1,17 +1,141 @@
-import { Text, View } from "react-native";
+import {
+  Alert,
+  Text,
+  TouchableOpacity,
+  useWindowDimensions,
+  View,
+} from "react-native";
+import * as Haptics from "expo-haptics";
+
+import { WORDS } from "@/DATA/CONSTANTS";
+
+const { ANSWER_RIGTH, ANSWER_WRONG, WORD_TO_TEST, TEST_POSITION } = WORDS[3];
+const WORD_ARRAY = WORD_TO_TEST.split("");
+const RANDOM = Math.random().toFixed(2);
+const ANSWER_UP = +RANDOM > 0.5 ? ANSWER_RIGTH : ANSWER_WRONG;
+const ANSWER_DOWN = +RANDOM <= 0.5 ? ANSWER_RIGTH : ANSWER_WRONG;
 
 export default function Index() {
+  const { width, height } = useWindowDimensions();
+  const MAX_WORD_LENGTH = 11;
+  const PADDING = 30;
+  const SIZE = (width - PADDING * 2) / MAX_WORD_LENGTH;
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        flexDirection: "row",
+        backgroundColor: "#ffffff",
       }}
     >
-      <Text style={{ fontSize: 100, fontFamily: "Nunito_800ExtraBold" }}>
-        МОЛОКО
-      </Text>
+      {WORD_ARRAY.map((l, i) => {
+        return (
+          <View
+            key={i}
+            style={{
+              width: SIZE,
+              height: SIZE,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: SIZE * 0.68,
+                fontFamily: "Nunito_800ExtraBold",
+              }}
+            >
+              {l}
+            </Text>
+          </View>
+        );
+      })}
+      <View
+        style={{
+          position: "absolute",
+          width: SIZE,
+          height: SIZE * 2,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#ffffff",
+          transform: [
+            {
+              translateX:
+                -((WORD_ARRAY.length / 2) * SIZE - SIZE / 2) +
+                SIZE * TEST_POSITION,
+            },
+          ],
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            if (ANSWER_UP === ANSWER_RIGTH) {
+              Haptics.notificationAsync(
+                Haptics.NotificationFeedbackType.Success
+              );
+              Alert.alert("ВЕРНЫЙ ОТВЕТ");
+            } else {
+              Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+              Alert.alert("НЕВЕРНЫЙ ОТВЕТ");
+            }
+          }}
+          style={{
+            width: SIZE,
+            height: SIZE,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: SIZE * 0.68,
+              fontFamily: "Nunito_800ExtraBold",
+              color: "rgba(0,0,0,0.2)",
+            }}
+          >
+            {ANSWER_UP}
+          </Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            width: SIZE,
+            height: 7,
+            borderRadius: 3.5,
+            backgroundColor: "rgba(0,0,0,0.2)",
+          }}
+        ></View>
+        <TouchableOpacity
+          onPress={() => {
+            if (ANSWER_DOWN === ANSWER_RIGTH) {
+              Haptics.notificationAsync(
+                Haptics.NotificationFeedbackType.Success
+              );
+              Alert.alert("ВЕРНЫЙ ОТВЕТ");
+            } else {
+              Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+              Alert.alert("НЕВЕРНЫЙ ОТВЕТ");
+            }
+          }}
+          style={{
+            width: SIZE,
+            height: SIZE,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: SIZE * 0.68,
+              fontFamily: "Nunito_800ExtraBold",
+              color: "rgba(0,0,0,0.2)",
+            }}
+          >
+            {ANSWER_DOWN}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
